dfae497e1738b2c89ec6484f2d17ca53d61ec9dd
Lames-Atef <lameatef53@gmail.com>
2023-08-30 16:51:14 +0300

    movie

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
-
+import React, { useState ,useEffect } from 'react'
+import axios from "axios"
+import styles from "./Home.scss"
 export default function Home() {
+  const[getTrend,setGetTrend]=useState([])
+  useEffect(()=>{
+getTrendingItem();
+  },[])
+  let getTrendingItem=async()=>{
+    let {data}=await axios.get("https://api.themoviedb.org/3/trending/all/day?api_key=262a3416c35895e97110287e19c8d5c2")
+  console.log(data)
+  setGetTrend(data.results)
+  }
   return (
-    <div>Home</div>
+    <>
+    <div className='container-fluid w-75'>
+    <div className='row gy-4 my-3 py-3'>
+      <div className='col-md-4'>
+<div className='brdr w-25 mb-4'></div>
+<h3>trending</h3>
+<h3>movie</h3>
+<h3>to watch now</h3>
+<span>most watched movie in day</span>
+<div className='brdr w-75 mt-4'></div>
+      </div>
+    {
+      getTrend.map((item,index)=>(
+<div key={index} className='col-md-2'>
+<div className='item'>
+  <img src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} className='w-100' alt="" />
+  <h5>{item.title}{item.name}</h5>
+</div>
+</div>
+      ))
+    }
+</div>
+    </div>
+    </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './Component/App/App';
 import reportWebVitals from './reportWebVitals';
-
+import "./index.scss"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
